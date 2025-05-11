fe8db1ce517e2e05cf227f62eb840c93516872fe
Vitor722 <vitor989santos@gmail.com>
2025-05-11 16:53:19 -0300

    StandAnlone to Legacy

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterModule.forRoot([])
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'SistemaFinanceiro' title`, () => {
+  it(`should have as title 'SistemaFinanceiro-Front-End'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('SistemaFinanceiro');
+    expect(app.title).toEqual('SistemaFinanceiro-Front-End');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, SistemaFinanceiro');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, SistemaFinanceiro-Front-End');
   });
 });
